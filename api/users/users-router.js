485e29238eabb33ab8a103ea18a4521b4feba079
Victor Tran <victor-likes-coding@tempestas.io>
2023-04-02 05:24:06 -0400

    write user router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,18 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const express = require('express');
 
+const router = express.Router();
+const { find } = require('./users-model');
+const { restricted } = require('../auth/auth-middleware');
+
+router.get('/', restricted, async (req, res, next) => {
+  try {
+    const users = await find();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
 
 /**
   [GET] /api/users
@@ -24,5 +37,5 @@
   }
  */
 
-
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
